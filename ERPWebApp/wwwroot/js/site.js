abd023e4e8517b6414bfa4391f36f71c302aa5c2
Tanzim Siddiqee <tanzimsiddiqee@hotmail.com>
2021-02-22 01:19:19 +0600

    initial commit

---
+++ ERPWebApp/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).on("click", "#pre", function (e) {
+    var $input = $(this).parents().find('.blinking');
+    var $preInput = $input.parent().parent().prev().children().find('input');
+    $input.removeClass('blinking');
+    $preInput.addClass('blinking');
+})
+
+$(document).on("click", "#nxt", function (e) {
+    var $input = $(this).parents().find('.blinking');
+    var $nxtInput = $input.parent().parent().next().children().find('input');
+    $input.removeClass('blinking');
+    $nxtInput.addClass('blinking');
+})
